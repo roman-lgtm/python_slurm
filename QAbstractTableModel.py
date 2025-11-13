5c2bddb8a38f0e98b484c63d1c32c68e9d672a59
roman.peregovor@gmail.com <roman.peregovor@gmail.com>
2025-11-13 12:26:10 +0300

    Add widget for work's with DB

---
+++ QAbstractTableModel.py
@@ -0,0 +1,54 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+# Исходные данные
+headers = ["Name", "Birthdate", "Contribution"]
+rows = [("Newton", "1643-01-04", "Classical mechanics"),
+        ("Einstein", "1879-03-14", "Relativity"),
+        ("Darwin", "1809-02-12", "Evolution")]
+
+
+class TableModel(QAbstractTableModel):
+    def __init__(self, data, headers, parent=None):
+        super(TableModel, self).__init__(parent)
+        self._data = data
+        self.headers = headers
+
+    def rowCount(self, parent):
+        return len(self._data)
+
+    def columnCount(self, parent):
+        return len(self.headers)
+
+    def data(self, index, role):
+        if role == Qt.ItemDataRole.DisplayRole:
+            return self._data[index.row()][index.column()]
+        return QVariant()
+
+    def headerData(self, section, orientation, role):
+        if role == Qt.ItemDataRole.DisplayRole and orientation == Qt.Orientation.Horizontal:
+            return self.headers[section]
+        return QVariant()
+
+    def setData(self, index, value, role):
+        if role == Qt.ItemDataRole.EditRole:
+            # Меняем данные в нашем списке _data
+            self._data[index.row()][index.column()] = value
+            # Оповещаем интерфейс о том, что данные изменены
+            self.dataChanged.emit(index, index, [Qt.ItemDataRole.DisplayRole])
+            return True
+        return False
+
+
+    def flags(self, index):
+        # Разрешаем редактирование + стандартные флаги
+        return Qt.ItemFlag.ItemIsSelectable | Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsEditable
+
+
+app = QApplication([])
+model = TableModel(rows, headers)
+view = QTableView()
+view.setModel(model)
+view.setEditTriggers(QAbstractItemView.DoubleClicked)
+view.show()
+app.exec()
