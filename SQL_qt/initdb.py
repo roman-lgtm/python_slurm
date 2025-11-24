635b3e84ba062b9808c0f04e3f8f8f42264ff3f6
Roman <74735602+roman-lgtm@users.noreply.github.com>
2025-11-24 19:29:32 +0300

    Update initdb.py

--- SQL_qt/initdb.py
+++ SQL_qt/initdb.py
@@ -1,8 +1,29 @@
-import sqlite3
-import random
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from sqlalchemy import MetaData, Table, String, Integer, Column, Text, DateTime, Boolean
+from datetime import datetime
 
-connection = sqlite3.connect("projects.db")
+metadata = MetaData()
+
+
+user = "postgres"
+password = "12345"
+
+
+#Создаем базу данных posgresql
+connection = psycopg2.connect(user=user, password=password, database=user, host="localhost", port=5432)
+connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cursor = connection.cursor()
+
+sql_create_database = cursor.execute("CREATE DATABASE IF NOT EXISTS roman_learn")
+cursor.close()
+connection.close()
+
+#Создаем енджин для создания таблиц и наполнения БД
+engine = create_engine("postgresql+psycopg2://postgres:12345@localhost/postgres")
+engine.connect()
+
+
 
 def random_int(start=1, end=100):
     a = random.randint(1, 100)
@@ -18,4 +39,3 @@ cursor.execute("INSERT INTO projects VALUES ('giraffes.io', 'Uber, but with gira
 for user in users_data:
     cursor.execute("INSERT INTO users(name , soname , age , gender , wt ) VALUES (:name , :soname , :age , :gender , :wt )",user)
 
-connection.commit()
