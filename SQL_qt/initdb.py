b4e18ad9bacb068eebd538464be3f1df23bd0720
Roman <74735602+roman-lgtm@users.noreply.github.com>
2025-11-25 16:03:34 +0300

    Update initdb.py

--- SQL_qt/initdb.py
+++ SQL_qt/initdb.py
@@ -10,27 +10,82 @@ metadata = MetaData()
 engine = create_engine("postgresql+psycopg2://postgres:12345@localhost/postgres")
 engine.connect()
 
-projects_table = Table('projects', metadata,
-Column('url', Text(), primary_key=True),
-    Column('descr', Text(), nullable=False),
+users_table = Table('users', metadata,
+Column('name', Text(), primary_key=True),
+    Column('soname', Text(), nullable=False),
     Column('income', Integer,  nullable=False)
 )
 
-users_table = Table('users', metadata,
-                    Column('id', Integer(), primary_key=True),
-                    Column('post_title', String(200), nullable=False),
-                    Column('post_slug', String(200), nullable=False),
-                    Column('content', Text(), nullable=False),
-                    Column('user_id', Integer(), ForeignKey("users.id"))
+
+Base = declarative_base()
+
+
+
+class YaProject(Base):
+    __tablename__ = 'ya_project'
+    id = Column(Integer, primary_key=True)
+    col1 = Column(Integer, nullable=False)
+    col2 = Column(Integer, nullable=False)
+    col3 = Column(Integer, nullable=False)
+    col4 = Column(Integer, nullable=False)
+    col5 = Column(Integer, nullable=False)
+
+    def fill_ya_projects(engine):
+        session = Session(bind=engine)
+
+        for i in range(100):
+            values = random.sample(range(0, 99999), 5)
+            session.add_all(
+                [
+                    YaProject(
+                        col1=values[0],
+                        col2=values[1],
+                        col3=values[2],
+                        col4=values[3],
+                        col5=values[4],
                     )
+                    for i in range(5)
+                ]
+            )
+            session.flush()
+        session.commit()
+    def init_db():
+        with engine.connect() as conn:
+            meta.drop_all(engine)
+            meta.create_all(engine)
+
+            conn.execute(insert(users_table),
+                {"name":"Vladimir",
+                "soname":"Barabaev",
+                "income":1200},
+                             {
+                                 "name":"Roman",
+                "soname":"Tarabaev",
+                "income":200
+                             },
+                             {
+                                 "name": "Sergey",
+                                 "soname": "Pizdabolov",
+                                 "income": 1200000
+                             }
+                             )
+            fill_ya_projects(engine)
 
 
 
-def random_int(start=1, end=100):
-    a = random.randint(1, 100)
-    return a
-#
-#
+    if __name__ == '__main__':
+        init_db()
+
+
+
+    # def random_int(start=1, end=100):
+    #     a = random.randint(1, 100)
+    #     return a
+
+
+
+metadata.create_all(engine)
+#СТАРЫЙ ВАРИАНТ ЧЕРЕЗ SQL_LIGHT
 # users_data  = [{'name':random_int(), 'soname':random_int(), 'age':random_int(), 'gender':random_int(),'wt':random_int()} for _ in range(100)]
 #
 # cursor.execute("""CREATE TABLE IF NOT EXISTS projects (url TEXT, descr TEXT, income INTEGER)""")
@@ -39,4 +94,3 @@ def random_int(start=1, end=100):
 #
 # for user in users_data:
 #     cursor.execute("INSERT INTO users(name , soname , age , gender , wt ) VALUES (:name , :soname , :age , :gender , :wt )",user)
-
